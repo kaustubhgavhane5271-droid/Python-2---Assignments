9a4e8ec4f50413b948681034d453f606442a5e1b
kaustubhgavhane5271-droid <kaustubhgavhane5271@gmail.com>
2026-02-10 21:10:21 +0530

    Add files via upload

---
+++ Assignment-3.py
@@ -0,0 +1,25 @@
+my_tup_1 = (12.5, 33.2, 45.6, 57.3)
+print(my_tup_1)
+
+# Accesing the tuple
+
+print(my_tup_1[1])
+
+my_tup_2 = (45, 32, 54, 23)
+print(my_tup_2)
+
+# Nesting Of Tuples
+
+nested_tuple = (my_tup_1,my_tup_2)
+print(nested_tuple)
+
+# Concatinate Both Tuples
+
+Concatinated_Tuple = my_tup_1 + my_tup_2
+print(Concatinated_Tuple)
+
+# Repetition of tuples
+
+repeated_tuple = my_tup_1 * 3
+print(repeated_tuple)
+
