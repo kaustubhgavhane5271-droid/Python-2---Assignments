f02e4b8da0ceb8976cba46ea1f4ec9d14ca6e7d5
kaustubhgavhane5271-droid <kaustubhgavhane5271@gmail.com>
2026-02-10 21:11:30 +0530

    Add files via upload

---
+++ Assignment-5.py
@@ -0,0 +1,29 @@
+# Create Bank Account History System.
+
+class BankAccount:
+    def __init__(self, account_number, balance=0.0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited ₹{amount}. New balance: ₹{self.balance}")
+        else:
+            print("Invalid Deposit Amount.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrawn ₹{amount}. New balance: ₹{self.balance}")
+
+    def check_balance(self):
+        print(f"Account Balance: ₹{self.balance}")
+
+# Enter Account Details.
+
+print("Account Summary: ")
+account = BankAccount(7552, 253)
+account.deposit(198)
+account.withdraw(143)
+account.check_balance()
